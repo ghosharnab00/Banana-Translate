5d7e7f25345ccf56e90fcec1fa5c773dccb0b902
Arnab Ghosh <ghosharnab00@gmail.com>
2022-02-14 10:16:14 +0530

    Changed the entire HTML

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 var txtInput = document.querySelector(".user_input");
-console.log (txtInput);
-
 var trans = document.querySelector(".click_me");
+console.log(txtInput)
 
-function clickHandler(){
-    alert("clicked");
-    //document.querySelector("#txt-output").innerText = txtInput;
-}
+function clickHandler() {
+    alart("clicked");
+    console.log("input", txtInput.value);
+};
 
-//trans.addEventListener("click", clickHandler);
-
-
-function showOutput(){
-    if (txtInput== null || txtInput == ""){
-        alert("no input");
-    }
-    else {
-        alert(txtInput);
-        document.getElementById("txt-output").innerText = txtInput;
-    }
-}
+trans.addEventListener("click", clickHandler);
