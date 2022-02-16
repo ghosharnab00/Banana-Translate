2a1dc2069730cffa440613d29ce0f4ef683c8237
Arnab Ghosh <ghosharnab00@gmail.com>
2022-02-17 01:54:16 +0530

    Finished the logic.

--- app.js
+++ app.js
@@ -2,13 +2,30 @@ var txtInput = document.querySelector("#user-input");
 var trans = document.querySelector("#translate");
 var output = document.querySelector("#output");
 
-var api = "https://api.funtranslations.com/translate/klingon.json";
-console.log(txtInput.textContent)
+console.log(txtInput.textContent);
 
-function clickHandler() {
-        console.log("input", txtInput.value);
-        output.innerHTML = txtInput.value;
+//var testApi = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var url = "https://api.funtranslations.com/translate/klingon.json";
+
+function getReq(url){
+        return (url + "?text=" + txtInput.value);
 }
 
+//function clickHandler() {
+  //      fetch (getReq(testApi))
+    //    .then(function res(response){ return response.json()})
+      //  .then( function js(json) { var output_text = json.contents.translated; output.innerHTML = output_text })
 
-trans.addEventListener("click", clickHandler);
+//}
+
+function callBack(){
+        fetch (getReq(url))
+        .then(response => response.json())
+        .then( json => {var output_text = json.contents.translated;
+                output.innerHTML = output_text; })
+                .catch(err => alert("Server Limit Crossed , try after 1 hour"))
+        }
+
+
+        trans.addEventListener("click", callBack);
